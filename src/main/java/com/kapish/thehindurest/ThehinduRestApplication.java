c5c285032964c5601af3f8e9f705955d7d1c7476
kspkidd <kapish.psingh.civ12@itbhu.ac.in>
2018-12-26 23:26:33 +0530

    initial commit

---
+++ src/main/java/com/kapish/thehindurest/ThehinduRestApplication.java
@@ -0,0 +1,14 @@
+package com.kapish.thehindurest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThehinduRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThehinduRestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kapish/thehindurest/ThehinduRestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kapish.thehindurest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThehinduRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
